0edb7ec24917453e2877360a601ea2820940eff5
Luisquii <luisquiiroga96@gmail.com>
2019-05-27 01:09:09 -0500

    Create ocv-PhotoFromVideo.py

---
+++ ocv-PhotoFromVideo.py
@@ -0,0 +1,27 @@
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+muestra = 0
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+
+
+    #Capture Frame
+    if cv2.waitKey(1) & 0xFF == ord('s'):
+        cv2.imwrite("muestra" +str(muestra) +".png", frame)
+        print("Muestra" +str(muestra) +" Saved")
+        muestra = muestra + 1
+
+
+    # Display the resulting frame
+    cv2.imshow('frame',frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
